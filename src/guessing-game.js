616927e26ecf502c0ff8ec32ba307bad9d3c6a14
Timur Yermakou <darkday3@mail.ru>
2019-10-03 18:22:07 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.mmin=min;
+        this.mmax=max;
     }
 
     guess() {
-
+        return Math.ceil((this.mmin+this.mmax)/2);
     }
 
     lower() {
-
+        this.mmax=this.guess();
     }
 
     greater() {
-
+        this.mmin=this.guess();
     }
 }
 
